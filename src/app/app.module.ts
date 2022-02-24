f4fdb6a3ef6b7b66021a127e38c89930f53ad292
nachodiaz <ignaciosergiodiaz@gmail.com>
2022-02-24 01:48:48 -0300

    mequeta scss actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,18 @@ import { User } from './models/user';
 
 import { HttpClientModule } from '@angular/common/http';
 import { UsereditComponent } from './components/useredit/useredit.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { SongComponent } from './components/song/song.component';
+import { AlbumComponent } from './components/album/album.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsereditComponent
+    UsereditComponent,
+    ArtistComponent,
+    SongComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
